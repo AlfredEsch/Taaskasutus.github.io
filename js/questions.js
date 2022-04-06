28a2c922a58d31ded805fe30e6b54b0a5d5327c7
AlfredEsch <alfred.eschbaum@khk.ee>
2022-04-06 09:51:51 +0300

    taaskasutus

---
+++ js/questions.js
@@ -0,0 +1,156 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Mitu aastat kulub ühe alumiiniumpurgi lagunemiseks?",
+    answer: "80-100 aastat",
+    options: [
+      "20-40 aastat",
+      "60-80 aastat",
+      "80-100 aastat",
+      "100-120 aastat"
+
+    ]
+  },
+    {
+    numb: 2,
+    question: "Mitu puud raiutakse maha, et teha üks tonn ajalehti?",
+    answer: "24 puud",
+    options: [
+      "15 puud",
+      "18 puud",
+      "21 puud",
+      "24 puud"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Taaskasutatud papi valmistamiseks kulub ... vähem energiat kui ringlussevõtmata papi valmistamiseks",
+    answer: "75%",
+    options: [
+      "75%",
+      "60%",
+      "80%",
+      "55%"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Mitu protsenti plastikut kasutatakse üks kord ja siis visatakse minema?",
+    answer: "40-50%",
+    options: [
+      "20-30%",
+      "40-50%",
+      "60-70%",
+      "80-90%"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Millist järgmistest ei saa taaskasutada? ",
+    answer: "Kõiki neist saab taaskasutada",
+    options: [
+      "Piimapakid",
+      "Plastikust veepudelid",
+      "Klaasist anumad",
+      "Kõiki neist saab taaskasutada"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Mitu korda saab klaasi taaskasutada ?",
+    answer: "Lõpmatult kordi",
+    options: [
+      "Klaasi ei saa taaskasutada ",
+      "Ühe korra",
+      "Umbes neli korda",
+      "Lõpmatult kordi"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Mida kasutatakse taaskasutamises metallide sorteerimiseks?",
+    answer: "Magnetid",
+    options: [
+      "Magnetid",
+      "Inimesed",
+      "Lintkonveier",
+      "Neid ei sorteerita"
+    ]
+  },
+
+    {
+    numb: 8,
+    question: "Mitu korda saab alumiiniumi taaskasutada?",
+    answer: "Nii palju kui tahad",
+    options: [
+      "Nii palju kui tahad",
+      "Viis korda",
+      "Ühe korra",
+      "Kümme korda"
+
+    ]
+
+  },
+
+    {
+      numb: 9,
+      question: "Mitu miljonit kilekotti kasutavad eestlased aastas kokku?",
+      answer: "700 miljonit",
+      options: [
+        "130 miljonit",
+        "300 miljonit",
+        "500 miljonit",
+        "700 miljonit"
+      ]
+
+  },
+
+    {
+      numb: 10,
+      question: "Vanast paberist uue tegemise käigus kulub ...",
+      answer: "80% vähem vett",
+      options: [
+          "35% vähem vett",
+          "50% vähem vett",
+          "80% vähem vett",
+          "90% vähem vett"
+      ]
+  },
+    {
+      numb: 11,
+      question: "Taaskasutatavad asjad on hea enne prügikasti panemist kotti panna",
+      answer: "Ei",
+      options: [
+          "Jah",
+          "Ei",
+	  "Jah",
+	  "Ei"
+          
+          
+
+      ]
+  },
+    {
+      numb: 12,
+      question: "Kui taaskasutatavad materjalid on kokku kogutud ja ringlussevõtu sorteerimisjaama transporditud, on need:",
+      answer: "Kõik ülaltoodud",
+      options: [
+          "Käsitsi sorteeritud probleemsete materjalide eemaldamiseks.",
+          "Mehaaniliselt sorteeritud magnetite, ekraanide ja optiliste sorteerijatega.",
+          "Kokkupressitakse suurteks tellisteks, mida nimetatakse pallideks ja müüakse ettevõtetele, kes puhastavad ja muudavad need uuteks toodeteks.",
+          "Kõik ülaltoodud"
+      ]
+  },
+    {
+      numb: 13,
+      question: "Mis on taaskasutus?",
+      answer: "Vanade toodete muutmine uuteks toodeteks",
+      options: [
+          "Asjade korduvkasutamine",
+          "Oma kotid toidupoodi kaasa toomine",
+          "Vanade toodete muutmine uuteks toodeteks",
+          "Asjade prügikasti panemine	"
+      ]
+  },
+];
